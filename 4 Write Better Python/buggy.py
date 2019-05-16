0210adc72caec7e99da9a1b142a6072862d41495
chris.givan <chrisgivan502@gmail.com>
2019-05-16 16:31:03 -0400

    Write Better Python

---
+++ 4 Write Better Python/buggy.py
@@ -0,0 +1,8 @@
+my_list = [5, 2, 1, True, "abcdefg", 3, False, 4]
+
+# import pdb; pdb.set_trace()
+    # n to goto next step, c to continue
+del my_list[3]
+del my_list[3]
+del my_list[4]
+print(my_list)
